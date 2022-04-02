17682df620416d1c2e05d443c826fc43d08f6000
Aksel Uhr <akseluhr8@gmail.com>
2022-04-02 14:13:09 +0200

    Created heading component

---
+++ components/Heading.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import styles from '../styles/HeadingStyle';
+
+const MainHeader = () => {
+  return (
+    <View>
+      <Text style={styles.bigBlue}>CityPops</Text>
+    </View>
+  )
+}
+export default MainHeader;
---
+++ styles/HeadingStyle.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    bigBlue: {
+      fontWeight: 'bold',
+      fontSize: 30,
+      top: -300,
+    },
+  });
+export default styles
