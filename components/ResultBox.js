af25ecfc18a79c7072b35c90f3d7ac3d1e6c8797
Aksel Uhr <akseluhr8@gmail.com>
2022-04-02 22:47:48 +0200

    Created result component
    Created a result box component where the results will be presented to the user.

---
+++ components/ResultBox.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import styles from '../styles/ResultBoxStyle';
+
+const ResultBox = () => {
+  return (
+    <View style={styles.viewContainer}>
+      <Text style={styles.resultText}>TODO: Add results</Text>
+    </View>
+  )
+}
+export default ResultBox;
