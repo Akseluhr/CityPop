4838a366b1cf0724c00c45b1f9496cfdd558bda8
Aksel Uhr <akseluhr8@gmail.com>
2022-04-05 17:12:02 +0200

    Added some navigation
    Expo cli bugging

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import SearchCityScreen from './screens/SearchCityScreen';
+import SearchCountryScreen from './screens/SearchCountryScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="SearchCity" component={SearchCityScreen} />
+          <Stack.Screen name="SearchCountry" component={SearchCountryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,11 +4,14 @@ import AppButton from "../components/AppButton";
 //import styles from "../styles/ButtonsStyle"
 import styles from "../styles/ViewStyle"
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
     return (
         <View style={styles.viewContainer}>
           <Heading   text="CityPop" />
-          <AppButton icon = {true} text="Search by city" />
+          <AppButton 
+            text="Search by city" 
+            onPress={() => navigation.navigate('SearchCity')}
+            />
           <AppButton text="Search by country" />
        </View>
     )
