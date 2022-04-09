e40ce91b27f95742423ff991ce7b8704165a953f
Aksel Uhr <akseluhr8@gmail.com>
2022-04-09 22:01:11 +0200

    Fixed positioning

--- styles/ViewStyle.js
+++ styles/ViewStyle.js
@@ -7,8 +7,7 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    marginTop: '-10%',
-    top: 100
+    marginTop: '-15%',
   }
 })
 
